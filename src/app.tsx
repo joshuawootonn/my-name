9d0b02d854c256e3619f86263f3d1fef9485bc91
Joshua Wootonn <jose56wonton@gmail.com>
2020-03-26 21:30:18 -0500

    chore: lint with 4 spaces

--- src/app.tsx
+++ src/app.tsx
@@ -2,36 +2,36 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Josher from './components/josher';
 import Navigation from './components/navigation';
-import Josher2 from "./components/josher2";
-import Brand from "./components/brand";
+import Josher2 from './components/josher2';
+import Brand from './components/brand';
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  margin: 0;
-  overflow: hidden;
-  justify-content: center;
-  align-items: center;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    margin: 0;
+    overflow: hidden;
+    justify-content: center;
+    align-items: center;
 `;
 
 export type AppState = 'josher' | 'josher2';
 
 const App: React.FC = () => {
-  const [currentAppState, setCurrentAppState] = useState<AppState>('josher');
+    const [currentAppState, setCurrentAppState] = useState<AppState>('josher');
 
-  console.log(currentAppState);
-  return (
-    <Container>
-      <Navigation
-        currentAppState={currentAppState}
-        setCurrentAppState={setCurrentAppState}
-      />
-      {currentAppState === 'josher' && <Josher />}
-      {currentAppState === 'josher2' && <Josher2 />}
-      <Brand />
-    </Container>
-  );
+    console.log(currentAppState);
+    return (
+        <Container>
+            <Navigation
+                currentAppState={currentAppState}
+                setCurrentAppState={setCurrentAppState}
+            />
+            {currentAppState === 'josher' && <Josher />}
+            {currentAppState === 'josher2' && <Josher2 />}
+            <Brand />
+        </Container>
+    );
 };
 
 export default App;
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,42 +3,42 @@ import styled from 'styled-components';
 import { AppState } from '../app';
 
 const Button = styled.button<{ active: boolean }>`
-  background-color: ${props => (props.active ? '#fff' : '#ddd')};
+    background-color: ${props => (props.active ? '#fff' : '#ddd')};
 `;
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  top: 16px;
-  position: absolute;
-  left: 16px;
-  button {
-    margin-bottom: 16px;
-  }
+    display: flex;
+    flex-direction: column;
+    top: 16px;
+    position: absolute;
+    left: 16px;
+    button {
+        margin-bottom: 16px;
+    }
 `;
 
 interface NavigationProps {
-  currentAppState: AppState;
-  setCurrentAppState: (appState: AppState) => void;
+    currentAppState: AppState;
+    setCurrentAppState: (appState: AppState) => void;
 }
 
 const Navigation: React.FC<NavigationProps> = props => {
-  console.log(props);
-  return (
-    <Wrapper>
-      <Button
-        active={'josher' === props.currentAppState}
-        onClick={() => props.setCurrentAppState('josher')}
-      >
-        11/5 Josher
-      </Button>
-      <Button
-        active={'josher2' === props.currentAppState}
-        onClick={() => props.setCurrentAppState('josher2')}
-      >
-        11/6 Josher2
-      </Button>
-    </Wrapper>
-  );
+    console.log(props);
+    return (
+        <Wrapper>
+            <Button
+                active={'josher' === props.currentAppState}
+                onClick={() => props.setCurrentAppState('josher')}
+            >
+                11/5 Josher
+            </Button>
+            <Button
+                active={'josher2' === props.currentAppState}
+                onClick={() => props.setCurrentAppState('josher2')}
+            >
+                11/6 Josher2
+            </Button>
+        </Wrapper>
+    );
 };
 
 export default Navigation;
