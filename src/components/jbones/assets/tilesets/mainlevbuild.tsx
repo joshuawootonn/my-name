5d3985c61ef5192c9eda6840a5b51cd0b5d59916
Joshua Wootonn <jose56wonton@gmail.com>
2020-04-02 21:37:00 -0500

    chore: reformat map and remove some bloat assets

--- src/components/jbones/assets/tilesets/mainlevbuild.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="mainlevbuild" tilewidth="16" tileheight="16" tilecount="2624" columns="64">
- <image source="../images/mainlevbuild.png" width="1024" height="656"/>
-</tileset>
---
+++ src/components/jbones/assets/tilesets/plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="plants" tilewidth="16" tileheight="16" tilecount="1728" columns="48">
+ <image source="../images/plants.png" width="768" height="576"/>
+</tileset>
